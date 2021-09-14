b18779a3ca94e2b66961208caf264de51e98852b
Momodu Ismaila <momodu.ismaila@centricityinsights.com>
2021-09-14 21:25:41 +0100

    commit

--- src/util/util.ts
+++ src/util/util.ts
@@ -11,14 +11,15 @@ import Jimp = require('Jimp');
 export async function filterImageFromURL(inputURL: string): Promise<string>{
     return new Promise( async resolve => {
         const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img: any)=>{
-            resolve(__dirname+outpath);
-        });
+        var random = Math.floor(Math.random() * 2000);
+        const outpath = `/tmp/filtered. ${random} .jpg`;
+        photo
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(__dirname + outpath, (img: any) => {
+                resolve(__dirname + outpath);
+            });
     });
 }
 
--- www/util/util.js
+++ www/util/util.js
@@ -24,7 +24,7 @@ function filterImageFromURL(inputURL) {
     return __awaiter(this, void 0, void 0, function* () {
         return new Promise((resolve) => __awaiter(this, void 0, void 0, function* () {
             const photo = yield Jimp.read(inputURL);
-            const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+            const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
             yield photo
                 .resize(256, 256) // resize
                 .quality(60) // set JPEG quality
